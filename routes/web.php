3266794a169543e6c162c5ac04e2a5e180cfb954
FelixOgenGelael <felixogengelael@gmail.com>
2025-11-14 08:36:28 +0700

    Add view and controller for user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Membuat alamat halaman baru
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/user/{id}', [HomeController::class, 'show'])->name('users.show');
+// name digunakan untuk memberi nama pada sebuah alamat
+
+Route::get('/fitur', function () {
+    return view('feature');
+})->name('feature');
