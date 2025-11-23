605649a0c96d453680d270cf8a2b22a62541bd50
FelixOgenGelael <felixogengelael@gmail.com>
2025-11-23 07:53:16 +0700

    Menambah Spitae permission

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -9,15 +9,23 @@ class BookPolicy
     /**
      * Create a new policy instance.
      */
+
+    //Create book
     public function create(User $User)
     {
-        //
         return $User->hasAnyRole(['admin', 'staff']);
     }
 
+    //Update book
     public function update(User $User)
     {
-        //
         return $User->hasRoleAnyRole('admin');
     }
+
+    //Delete book
+    public function delete(User $User)
+    {
+        return $User->hasRoleAnyRole('admin');
+    }
+
 }
